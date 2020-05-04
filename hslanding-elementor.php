455a894a3da6280270d9c61a4d40b201c335db85
nguyen_dttn <contact.wilcity@gmail.com>
2020-05-04 18:27:16 +0700

    First commit

---
+++ hslanding-elementor.php
@@ -0,0 +1,9 @@
+<?php
+/*
+ * Plugin Name: Hs Landing Elementor
+ * Plugin URI: https://wiloke.com
+ * Author: wiloke
+ * Author URI: https://wiloke.com
+ * Description: HPBuilder Builder
+ * Version: 1.0
+ */
